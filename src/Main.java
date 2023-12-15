b497969f03201ff3f2ab0da2e2c5a62c11fffd24
BT_1N3_01 <sample@gmail.com>
2023-12-15 09:13:03 -0500

    edited print statement

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("yes");
+        System.out.println("yes yes");
         System.out.println("person2 yes");
         System.out.println("no");
         System.out.println("Part 6 step 64 changes");
